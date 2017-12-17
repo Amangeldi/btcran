72a0d822af450ab89d679e8a3cc58a287d1d1801
Amangeldi <Amangeldi.9606@gmail.com>
2017-12-17 20:50:56 +0500

    index php перевел кодировку на utf-8 без BOM. А homepage на utf-8 для отображения.

---
+++ www/homepage.php
@@ -0,0 +1,22 @@
+<?php
+$STR=<<<ABC
+<html>
+    <head>
+        <meta charset="utf-8">
+		<link href="style.css" rel="stylesheet">
+        <title>Биткоин кран</title>
+    </head>
+    <body>
+        <div class="header">Логотип и меню</div>
+		<div class="layout">
+			<div class="chat">чат</div>
+
+			<div class="coins-list">Список валют</div>
+
+			<div class="right-column">Основное содержание страницы</div>
+		</div>
+        <div class="footer">Подвал</div>
+    </body>
+</html>
+ABC;
+?>
--- www/index.php
+++ www/index.php
@@ -1,25 +1,6 @@
-﻿<?php
+<?php
 session_start();
-$STR=<<<ABC
-<html>
-    <head>
-        <meta charset="utf-8">
-		<link href="style.css" rel="stylesheet">
-        <title>Биткоин кран</title>
-    </head>
-    <body>
-        <div class="header">Логотип и меню</div>
-		<div class="layout">
-			<div class="chat">чат</div>
-
-			<div class="coins-list">Список валют</div>
-
-			<div class="right-column">Основное содержание страницы</div>
-		</div>
-        <div class="footer">Подвал</div>
-    </body>
-</html>
-ABC;
+include "homepage.php";
 if (empty($_SESSION['login']) or empty($_SESSION['id']))
 {
 	echo '<script>location.replace("login.php");</script>'; exit;
